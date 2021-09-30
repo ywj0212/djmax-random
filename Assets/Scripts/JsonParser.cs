ea088018b7f55105b8dc2a419ea043d072bd66f4
유우진 <ywj02120212@gmail.com>
2021-09-30 17:23:43 +0900

    v4

--- Assets/Scripts/JsonParser.cs
+++ Assets/Scripts/JsonParser.cs
@@ -13,6 +13,7 @@ public class TrackData {
 public class Track {
     public string Ctgr;
     public string Name;
+    public string Cmps;
     public string Bt;
     public string Diff;
     public int Lv;
@@ -24,14 +25,85 @@ public class JsonParser : MonoBehaviour
     [Space]
     [SerializeField] private Transform musicListUIParent;
     [SerializeField] private GameObject musicListPrefab;
+    [Space]
+    [SerializeField] private Transform DiffParent;
+    [SerializeField] private Transform LvParent;
+    [SerializeField] private Transform DLCParent1;
+    [SerializeField] private Transform DLCParent2;
+    [SerializeField] private Text CountLabel;
 
     private bool    _4b = true, _5b = true, _6b = true, _8b = true,
                     _nm = true, _hd = true, _mx = true, _sc = true,
                     _1 = true, _2 = true, _3 = true, _4 = true, _5 = true, 
                     _6 = true, _7 = true, _8 = true, _9 = true, _10 = true, 
                     _11 = true, _12 = true, _13 = true, _14 = true, _15 = true;
+    
+    private bool    _RE = true, _P1 = true, _P2 = true, _P3 = true,
+                    _T1 = true, _T2 = true, _T3 = true,
+                    _BS = true, _CE = true, _TR = true,
+                    _VE = true, _ES = true, _CH = true, _GC = true,
+                    _DM = true, _CY = true, _EM = true,
+                    _GF = true, _GG = true;
+    
+    private int _Count = 8;
+
 #region UI
     public void _StartBtn() { ShowTracks(); }
+
+    public void _U_Count(Slider s) { _Count = (int)s.value; CountLabel.text = ((int)s.value).ToString(); }
+
+    public void _DIFF_ALL() {
+        foreach(Transform t in DiffParent) {
+            Toggle tg = t.GetComponent<Toggle>();
+            if(tg != null)
+                tg.isOn = true;
+        }
+    }
+    public void _DIFF_NONE() {
+        foreach(Transform t in DiffParent) {
+            Toggle tg = t.GetComponent<Toggle>();
+            if(tg != null)
+                tg.isOn = false;
+        }
+    }
+    public void _LV_ALL() {
+        foreach(Transform t in LvParent) {
+            Toggle tg = t.GetComponent<Toggle>();
+            if(tg != null)
+                tg.isOn = true;
+        }
+    }
+    public void _LV_NONE() {
+        foreach(Transform t in LvParent) {
+            Toggle tg = t.GetComponent<Toggle>();
+            if(tg != null)
+                tg.isOn = false;
+        }
+    }
+    public void _DLC_ALL() {
+        foreach(Transform t in DLCParent1) {
+            Toggle tg = t.GetComponent<Toggle>();
+            if(tg != null)
+                tg.isOn = true;
+        }
+        foreach(Transform t in DLCParent2) {
+            Toggle tg = t.GetComponent<Toggle>();
+            if(tg != null)
+                tg.isOn = true;
+        }
+    }
+    public void _DLC_NONE() {
+        foreach(Transform t in DLCParent1) {
+            Toggle tg = t.GetComponent<Toggle>();
+            if(tg != null)
+                tg.isOn = false;
+        }
+        foreach(Transform t in DLCParent2) {
+            Toggle tg = t.GetComponent<Toggle>();
+            if(tg != null)
+                tg.isOn = false;
+        }
+    }
 
     public void _4B(bool b) { _4b = b; }
     public void _5B(bool b) { _5b = b; }
@@ -58,6 +130,26 @@ public class JsonParser : MonoBehaviour
     public void _Lv13(bool b) { _13 = b; }
     public void _Lv14(bool b) { _14 = b; }
     public void _Lv15(bool b) { _15 = b; }
+
+    public void _D_RE(bool b) { _RE = b; }
+    public void _D_P1(bool b) { _P1 = b; }
+    public void _D_P2(bool b) { _P2 = b; }
+    public void _D_P3(bool b) { _P3 = b; }
+    public void _D_T1(bool b) { _T1 = b; }
+    public void _D_T2(bool b) { _T2 = b; }
+    public void _D_T3(bool b) { _T3 = b; }
+    public void _D_BS(bool b) { _BS = b; }
+    public void _D_CE(bool b) { _CE = b; }
+    public void _D_TR(bool b) { _TR = b; }
+    public void _D_VE(bool b) { _VE = b; }
+    public void _D_ES(bool b) { _ES = b; }
+    public void _D_CH(bool b) { _CH = b; }
+    public void _D_GC(bool b) { _GC = b; }
+    public void _D_DM(bool b) { _DM = b; }
+    public void _D_CY(bool b) { _CY = b; }
+    public void _D_EM(bool b) { _EM = b; }
+    public void _D_GF(bool b) { _GF = b; }
+    public void _D_GG(bool b) { _GG = b; }
 #endregion
     
     private TrackData trackData;
@@ -67,7 +159,7 @@ public class JsonParser : MonoBehaviour
     }
 
     public void ShowTracks() {
-        List<Track> tracks = GetTracks();
+        List<Track> tracks = GetTracks(count:_Count);
         
         foreach(Transform t in musicListUIParent) {
             Destroy(t.gameObject);
@@ -106,13 +198,32 @@ public class JsonParser : MonoBehaviour
             temp.transform.GetChild(1).GetChild(2).GetComponent<Text>().text = t.Name;
 
             // ? Track > Description > Info(Text)
-            temp.transform.GetChild(1).GetChild(3).GetComponent<Text>().text = t.Bt + " " + t.Diff + "  Lv." + t.Lv.ToString();
+            temp.transform.GetChild(1).GetChild(3).GetComponent<Text>().text = t.Cmps;
 
-            // ? Track > Description > LevelToggle
-            Transform levelToggleParent = temp.transform.GetChild(1).GetChild(4);
-            for(int i = 0; i < t.Lv; i++) {
-                levelToggleParent.GetChild(i).GetComponent<Toggle>().isOn = true;
+            if(t.Diff == "SC") {
+                // ? Track > Description > LevelToggle SC
+                Transform levelToggleParent = temp.transform.GetChild(1).GetChild(4);
+                Transform levelToggleParentSC = temp.transform.GetChild(1).GetChild(5);
+                
+                levelToggleParent.gameObject.SetActive(false);
+                levelToggleParentSC.gameObject.SetActive(true);
+
+                for(int i = 0; i < t.Lv; i++) {
+                    levelToggleParentSC.GetChild(i).GetComponent<Toggle>().isOn = true;
+                }
             }
+            else {
+                // ? Track > Description > LevelToggle
+                Transform levelToggleParent = temp.transform.GetChild(1).GetChild(4);
+                Transform levelToggleParentSC = temp.transform.GetChild(1).GetChild(5);
+
+                levelToggleParent.gameObject.SetActive(true);
+                levelToggleParentSC.gameObject.SetActive(false);
+
+                for(int i = 0; i < t.Lv; i++) {
+                    levelToggleParent.GetChild(i).GetComponent<Toggle>().isOn = true;
+                }
+            }         
         }
     }
 
@@ -120,9 +231,20 @@ public class JsonParser : MonoBehaviour
 
         List<Track> pass = trackData.tracks.FindAll( (t) =>  
         (
-                ( (t.Bt.Equals("4B") && _4b) || (t.Bt.Equals("5B") && _5b) || (t.Bt.Equals("6B") && _6b) || (t.Bt.Equals("8B") && _8b) )
+               ( (t.Bt.Equals("4B") && _4b)   || (t.Bt.Equals("5B") && _5b)   || (t.Bt.Equals("6B") && _6b)   || (t.Bt.Equals("8B") && _8b) )
             && ( (t.Diff.Equals("NM") && _nm) || (t.Diff.Equals("HD") && _hd) || (t.Diff.Equals("MX") && _mx) || (t.Diff.Equals("SC") && _sc) )
-            && ( (t.Lv == 1 && _1) || (t.Lv == 2 && _2) || (t.Lv == 3 && _3) || (t.Lv == 4 && _4) || (t.Lv == 5 && _5) || (t.Lv == 6 && _6) || (t.Lv == 7 && _7) || (t.Lv == 8 && _8) || (t.Lv == 9 && _9) || (t.Lv == 10 && _10) || (t.Lv == 11 && _11) || (t.Lv == 12 && _12) || (t.Lv == 13 && _13) || (t.Lv == 14 && _14) || (t.Lv == 15 && _15) )
+            && (
+                 (t.Lv == 1 && _1)   || (t.Lv == 2 && _2)   || (t.Lv == 3 && _3)   || (t.Lv == 4 && _4)   || (t.Lv == 5 && _5)   ||
+                 (t.Lv == 6 && _6)   || (t.Lv == 7 && _7)   || (t.Lv == 8 && _8)   || (t.Lv == 9 && _9)   || (t.Lv == 10 && _10) ||
+                 (t.Lv == 11 && _11) || (t.Lv == 12 && _12) || (t.Lv == 13 && _13) || (t.Lv == 14 && _14) || (t.Lv == 15 && _15)
+               )
+            && (
+                 (t.Ctgr == "RE" && _RE) || (t.Ctgr == "P1" && _P1) || (t.Ctgr == "P2" && _P2) || (t.Ctgr == "P3" && _P3) || (t.Ctgr == "T1" && _T1) || (t.Ctgr == "T2" && _T2) || (t.Ctgr == "T3" && _T3) || 
+                 (t.Ctgr == "BS" && _BS) || (t.Ctgr == "CE" && _CE) || (t.Ctgr == "TR" && _TR) || 
+                 (t.Ctgr == "VE" && _VE) || (t.Ctgr == "ES" && _ES) || (t.Ctgr == "CH" && _CH) || (t.Ctgr == "GC" && _GC) || 
+                 (t.Ctgr == "DM" && _DM) || (t.Ctgr == "CY" && _CY) || (t.Ctgr == "EM" && _EM) || 
+                 (t.Ctgr == "GF" && _GF) || (t.Ctgr == "GG" && _GG)
+               )
         ) );
 
         pass = pass.OrderBy(a => Guid.NewGuid()).ToList();
